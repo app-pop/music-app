83ef1b23443e1ae90047d3618c400374fc358c2f
WIN-KN3VVTK6MKA\EDY <211977992@qq.com>
2026-05-18 14:59:39 +0800

    fix: avoid internal cli plugin import in rn config

--- react-native.config.js
+++ react-native.config.js
@@ -1,17 +1,7 @@
 ﻿const ios = require('@react-native-community/cli-platform-ios')
 const android = require('@react-native-community/cli-platform-android')
-const communityCliPlugin = require('@react-native/community-cli-plugin/dist/index.flow.js')
-
-const commands = [
-  ...ios.commands,
-  ...android.commands,
-  communityCliPlugin.bundleCommand,
-  communityCliPlugin.ramBundleCommand,
-  communityCliPlugin.startCommand,
-].filter(Boolean)
 
 module.exports = {
-  commands,
   platforms: {
     ios: {
       projectConfig: ios.projectConfig,
